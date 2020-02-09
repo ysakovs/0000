56b0164f80634b9591be211ea52a0bb6329345b0
unknown <papaaced@gmail.com>
2020-02-09 17:17:46 -0500

    delete

--- client/guitartab/src/components/BrowseBtn.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-btn to="/songs">
-    <span class="mr-2">Browse</span>
-  </v-btn>
-</template>
--- client/guitartab/vue.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-module.exports = {
-  outputDir: path.resolve(__dirname, '../../server/public'),
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
